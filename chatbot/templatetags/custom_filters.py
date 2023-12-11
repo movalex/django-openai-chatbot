7fd9818b4a0679294749644835a3e44e253f20ec
Alexey Bogomolov <mail@abogomolov.com>
2023-12-11 06:51:27 +0300

    add simple formatting filtering

---
+++ chatbot/templatetags/custom_filters.py
@@ -0,0 +1,28 @@
+from django import template
+from django.utils.html import format_html, mark_safe
+
+register = template.Library()
+
+
+@register.filter(name="format_output")
+def format_output(value):
+    # Splitting the text into lines
+    lines = value.split("\n")
+
+    # Variables to keep track of whether we are inside a code block
+    in_code_block = False
+    formatted_lines = []
+
+    for line in lines:
+        if line.strip().startswith("```"):  # Check for code block start/end
+            in_code_block = not in_code_block
+            if in_code_block:
+                formatted_lines.append("<pre><code>")
+            else:
+                formatted_lines.append("</code></pre>")
+        elif in_code_block:
+            formatted_lines.append(mark_safe(line + "\n"))  # Keep code line as is
+        else:
+            formatted_lines.append(f"<p>{line}</p>")
+    concatenated = "".join(formatted_lines)
+    return mark_safe(concatenated)
