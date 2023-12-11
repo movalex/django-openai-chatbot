e718c251de0f6eb4d6664ae88866191bcec4e629
Alexey Bogomolov <mail@abogomolov.com>
2023-12-11 06:51:08 +0300

    use chat context

--- chatbot/views.py
+++ chatbot/views.py
@@ -5,7 +5,7 @@ import openai
 
 from django.contrib import auth
 from django.contrib.auth.models import User
-from .models import Chat
+from .models import Chat, ChatSession
 
 from django.utils import timezone
 
@@ -15,17 +15,12 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = openai_api_key
 
 
-def ask_openai(message):
+def ask_openai(message, chat_context):
     response = openai.chat.completions.create(
         model="gpt-4-1106-preview",
-        messages=[
-            {"role": "system", "content": "You are an helpful assistant."},
-            {"role": "user", "content": message},
-        ],
+        messages=chat_context + [{"role": "user", "content": message}],
     )
-
-    answer = response.choices[0].message.content.strip()
-    return answer
+    return response
 
 
 # Create your views here.
@@ -37,16 +32,45 @@ def chatbot(request):
 
     if request.method == "POST":
         message = request.POST.get("message")
-        response = ask_openai(message)
+        # Retrieve or create a chat session
+        session_id = str(request.user.id)  # Using user ID as session identifier
+        chat_session, created = ChatSession.objects.get_or_create(session_id=session_id)
 
+        # Get current chat context
+        chat_context = eval(chat_session.context) if chat_session.context else []
+
+        # Get response from OpenAI
+        response = ask_openai(message, chat_context)
+        assistant_response = response.choices[
+            0
+        ].message.content.strip()  # Extracting the text from the response
+
+        # Update chat context
+        chat_context.append({"role": "user", "content": message})
+        chat_context.append(
+            {
+                "role": "assistant",
+                "content": assistant_response,
+            }
+        )
+
+        # Save the updated context
+        chat_session.context = str(chat_context)
+        chat_session.save()
+
+        # Save the chat message and response
         chat = Chat(
             user=request.user,
             message=message,
-            response=response,
+            response=assistant_response,
             created_at=timezone.now(),
         )
         chat.save()
-        return JsonResponse({"message": message, "response": response})
+
+        return JsonResponse({"message": message, "response": assistant_response})
+
+    chats = Chat.objects.filter(user=request.user)
+
     return render(request, "chatbot.html", {"chats": chats})
 
 
