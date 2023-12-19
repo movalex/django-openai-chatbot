7ed3f2c81c6460207f20d116eaf0daac3e774dd8
Alexey Bogomolov <mail@abogomolov.com>
2023-12-19 23:18:21 +0300

    add trim context

--- chatbot/views.py
+++ chatbot/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import os
 import openai
+import json
+import logging
+
 
 from django.contrib import auth
 from django.contrib.auth.models import User
@@ -16,6 +19,8 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 openai.api_key = openai_api_key
 
+logger = logging.getLogger(__name__)
+
 
 def ask_openai(message, chat_context):
     response = openai.chat.completions.create(
@@ -25,14 +30,12 @@ def ask_openai(message, chat_context):
     return response
 
 
-# Create your views here.
-@login_required(login_url='login')
+@login_required(login_url="login")
 def chatbot(request):
     chats = []
-
-    chats = Chat.objects.filter(user=request.user)
-    if request.user.is_authenticated:
-        chats = Chat.objects.filter(user=request.user)
+    MAX_CONTEXT_SIZE = 200
+    TRIM_CONTEXT = True
+    MAX_USED_CONTEXT = 100
 
     if request.method == "POST":
         message = request.POST.get("message")
@@ -41,14 +44,30 @@ def chatbot(request):
         chat_session, created = ChatSession.objects.get_or_create(session_id=session_id)
 
         # Get current chat context
-        chat_context = eval(chat_session.context) if chat_session.context else []
-
+        try:
+            chat_context = (
+                json.loads(chat_session.context) if chat_session.context else []
+            )
+        except json.JSONDecodeError:
+            chat_context = request.session.get("chat_context", [])
+        print(len(chat_context))
         # Get response from OpenAI
-        response = ask_openai(message, chat_context)
-        assistant_response = response.choices[
-            0
-        ].message.content.strip()  # Extracting the text from the response
+        chat_used_context = chat_context[-MAX_USED_CONTEXT*2:]
+        print(f"current context size: {len(chat_used_context)}")
+        try:
+            response = ask_openai(message, chat_used_context)
 
+        except openai.PermissionDeniedError:
+            logger.error("Permission denied to OpenAI services")
+            return JsonResponse(
+                {"error": "Permission denied to OpenAI services."}, status=403
+            )
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+        # Extracting the text from the response
+        assistant_response = response.choices[0].message.content.strip()
+        print(f"response: {assistant_response}")
         # Update chat context
         chat_context.append({"role": "user", "content": message})
         chat_context.append(
@@ -57,9 +76,14 @@ def chatbot(request):
                 "content": assistant_response,
             }
         )
+        # Trim the context if it exceeds the maximum size
+        if (
+            TRIM_CONTEXT and len(chat_context) > MAX_CONTEXT_SIZE * 2
+        ):  # *2 because each interaction has 2 parts
+            chat_context = chat_context[-MAX_CONTEXT_SIZE * 2 :]
 
         # Save the updated context
-        chat_session.context = str(chat_context)
+        chat_session.context = json.dumps(chat_context)
         chat_session.save()
 
         # Save the chat message and response
@@ -74,7 +98,6 @@ def chatbot(request):
         return JsonResponse({"message": message, "response": assistant_response})
 
     chats = Chat.objects.filter(user=request.user)
-
     return render(request, "chatbot.html", {"chats": chats})
 
 
