04d1087e1174f7b8afdf4650956478e8efa68610
Alexey Bogomolov <mail@abogomolov.com>
2024-01-01 22:06:45 +0300

    update highlightjs version

--- static/js/highlight.js
+++ static/js/highlight.js
@@ -1,6 +1,6 @@
 function loadHighlightJs() {
     var script = document.createElement('script');
-    script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js";
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js";
     script.onload = function () {
         hljs.highlightAll() // Initialize Highlight.js when the script is loaded
     };
