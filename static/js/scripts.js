bcadb6848d022fdb5648477f2c7bf72d7be5bb7a
Alexey Bogomolov <mail@abogomolov.com>
2024-01-08 07:12:28 +0300

    disable old sidebar script

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,22 +10,22 @@
 
 // console.log("you have to work")
 
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
-});
+//     // Toggle the side navigation
+//     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+//     if (sidebarToggle) {
+//         // Uncomment Below to persist sidebar toggle between refreshes
+//         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+//         //     document.body.classList.toggle('sb-sidenav-toggled');
+//         // }
+//         sidebarToggle.addEventListener('click', event => {
+//             event.preventDefault();
+//             document.body.classList.toggle('sb-sidenav-toggled');
+//             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+//         });
+//     }
+// });
 // function adjustChatListHeight() {
 //     var sidebarWrapper = document.getElementById('sidebar-wrapper');
 //     var headerHeight = sidebarWrapper.querySelector('.mb-3').offsetHeight; // Assuming this is the header
