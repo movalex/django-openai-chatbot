110be98065dedbbf8b9bee03aee42e6c587db174
Alexey Bogomolov <mail@abogomolov.com>
2024-01-14 22:01:31 +0300

    add markdown formatting test

--- chatbot/tests.py
+++ chatbot/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from chatbot.templatetags.custom_filters import markdown_to_html
 
-# Create your tests here.
+
+class FiltersTest(TestCase):
+    INP = """1. **HEADER1**: 
+
+   Example using vanilla JavaScript:
+   ```javascript
+   some-code {
+   };
+   ```
+2. **HEADER2**"""
+
+    def test_markdown_output(self):
+        assert (
+            markdown_to_html(self.INP)
+            == """<ol>
+<li><strong>HEADER1</strong>: </li>
+</ol>
+<p>Example using vanilla JavaScript:</p>
+<pre><code class="language-javascript">   some-code {
+   };
+
+</code></pre>
+<ol>
+<li><strong>HEADER2</strong></li>
+</ol>"""
+        )
