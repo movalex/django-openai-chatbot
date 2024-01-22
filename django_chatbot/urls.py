3dc1333cb16008b1d349c63672b5d3b0473b7a0b
Alexey Bogomolov <mail@abogomolov.com>
2024-01-23 01:27:08 +0300

    cleanup

--- django_chatbot/urls.py
+++ django_chatbot/urls.py
@@ -21,5 +21,4 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("chatbot.urls")),
-    path('bs/', TemplateView.as_view(template_name='sidebars.html')),
 ]
