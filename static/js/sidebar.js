dd4e5089d36b369ad16b48b490ca6eff82d3cfa9
Alexey Bogomolov <mail@abogomolov.com>
2024-02-16 03:17:21 +0300

    tweak sidebar toggle scripts

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,43 +1,6 @@
 const sidebarWrapper = document.getElementById('sidebar-wrapper');
-
-window.addEventListener('DOMContentLoaded', event => {
-    // Get the button 
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    const closeSidebar = document.body.querySelector('#closeButton');
-    
-    if (sidebarToggle || closeSidebar) {
-
-        // Add event listener for clicks on the toggle button
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            sidebarWrapper.classList.toggle('toggled');
-        });
-        closeSidebar.addEventListener('click', event => {
-            event.preventDefault();
-            sidebarWrapper.classList.toggle('toggled');
-        });
-    }
-});
-
-// Add click event listener to the document
-document.addEventListener('click', (event) => {
-
-    // Check if the click is on the toggle button or its children
-    if (sidebarToggle.contains(event.target)) {
-        event.preventDefault(); // Only call this if the click is on the toggle button
-        return;
-    }
-    // Check if the click is on the toggle button or its children
-    if (!sidebarWrapper || sidebarToggle.contains(event.target)) {
-        return;
-    }
-    // Check if the click is outside the sidebar
-    if (sidebarWrapper && !sidebarWrapper.contains(event.target)) {
-        // Close the sidebar
-        sidebarWrapper.classList.remove('toggled');
-    }
-});
-    
+const sidebarToggle = document.body.querySelector('#sidebarToggle');
+const closeSidebar = document.body.querySelector('#closeSidebarButton');
 
 function createNewChatRoom() {
     // Retrieve the CSRF token from the document
@@ -71,3 +34,67 @@ function createNewChatRoom() {
 }
 
 document.getElementById('createChatButton').addEventListener('click', createNewChatRoom);
+
+function toggleSidebarOnLoad() {
+
+        if (sidebarToggle || closeSidebar) {
+
+        // Add event listener for clicks on the toggle button
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            sidebarWrapper.classList.toggle('toggled');
+        });
+        closeSidebar.addEventListener('click', event => {
+            event.preventDefault();
+            sidebarWrapper.classList.toggle('toggled');
+        });
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetch and display the chat rooms list
+    fetchChatRooms();
+    toggleSidebarOnLoad();
+
+    // Other initial setup tasks can go here
+});
+
+
+function fetchChatRooms() {
+    fetch('/get_chat_rooms/')
+        .then(response => response.json())
+        .then(data => {
+            if(data.chat_rooms) {
+                populateChatRooms(data.chat_rooms);
+            } else {
+                console.error('Chat rooms data is missing');
+            }
+        })
+        .catch(e => {
+            console.error('Fetch error:', e);
+        });
+}
+
+
+function populateChatRooms(chatRooms) {
+    // Clear existing chat rooms
+    const listGroup = document.getElementById('list-group');
+    listGroup.innerHTML = '';   
+    // Append each chat room to the list
+    const currentPath = window.location.pathname;
+    const chatRoomIdOrUuid = currentPath.split('/')[2];
+    chatRooms.forEach(room => {
+        const anchor = document.createElement('a');
+        anchor.href = `/chatroom/${room.id}/`;  // Update with the correct URL pattern
+        anchor.className = 'list-group-item list-group-item-action list-group-item-light p-3';
+        const span = document.createElement('span');
+        anchor.appendChild(span);
+        span.textContent = room.name;
+        // If the room's ID/UUID matches the current URL, add the 'active-chatroom' class
+        if(room.id === chatRoomIdOrUuid) {
+            anchor.classList.add('active-chatroom');
+        }
+        listGroup.appendChild(anchor);
+    });
+}
