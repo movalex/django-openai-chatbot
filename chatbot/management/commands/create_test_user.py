56e7a6452277ed73e1d0599ed668bb465327cba4
Alexey Bogomolov <mail@abogomolov.com>
2024-02-22 22:43:17 +0300

    get the env

--- chatbot/management/commands/create_test_user.py
+++ chatbot/management/commands/create_test_user.py
@@ -6,14 +6,13 @@ from chatbot.models import ChatRoom
 
 class Command(BaseCommand):
     help = "Creating a default test user for debugging purposes"
-    print(help)
-    DJANGO_TEST_PASSWORD = os.getenv("DJANGO_TEST_PASSWORD")
+    DJANGO_TEST_PASSWORD = os.getenv("DJANGO_TEST_PASSWORD", "testtest")
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="testuser").exists():
             user = User.objects.create_user(
                 "alex", "testuser@example.com", self.DJANGO_TEST_PASSWORD
             )
-            ChatRoom.objects.create(name="Alex's Default Chat", user=user)
+            ChatRoom.objects.create(name="Default Chat", user=user)
         else:
             self.stdout.write(self.style.NOTICE("Test user already exists"))
