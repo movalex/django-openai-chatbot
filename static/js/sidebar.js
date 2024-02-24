5e492adb9ef50ef7515fc71fc713b59bab4e9873
Alexey Bogomolov <mail@abogomolov.com>
2024-02-24 03:49:50 +0300

    add archive room handling

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,10 +1,10 @@
 const sidebarWrapper = document.getElementById('sidebar-wrapper');
 const sidebarToggle = document.body.querySelector('#sidebarToggle');
 const closeSidebar = document.body.querySelector('#closeSidebarButton');
+var csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
 function createNewChatRoom() {
     // Retrieve the CSRF token from the document
-    var csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
     // Set up the request headers
     var headers = new Headers();
@@ -41,7 +41,6 @@ function isSidebarVisible() {
     return !document.getElementById('sidebar-wrapper').classList.contains('toggled');
 }
 
-
 // Function to toggle the sidebarToggle button visibility
 function toggleSidebarButtonVisibility() {
     const sidebarToggle = document.getElementById('sidebarToggle');
@@ -59,7 +58,6 @@ function toggleSidebarButtonVisibility() {
 
 function toggleSidebarOnLoad() {
     if (sidebarToggle || closeSidebar) {
-
         // Add event listener for clicks on the toggle button
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
@@ -74,11 +72,8 @@ function toggleSidebarOnLoad() {
     }
 }
 
-
-
 // Event listener for window resize
 window.addEventListener('resize', toggleSidebarButtonVisibility);
-
 
 function populateChatRooms(chatRooms) {
     // Clear existing chat rooms
@@ -90,24 +85,61 @@ function populateChatRooms(chatRooms) {
     chatRooms.forEach(room => {
         const anchor = document.createElement('a');
         anchor.href = `/chatroom/${room.id}/`;  // Update with the correct URL pattern
+        anchor.setAttribute('data-chat-id', room.id);
         anchor.className = 'list-group-item list-group-item-action list-group-item-light p-3';
+
+        // Create a container for the span and the button
+        const container = document.createElement('div');
+        container.style.display = 'flex';
+        container.style.justifyContent = 'space-between';
+        container.style.width = '100%';
+        container.style.alignItems = 'center';
+
         const span = document.createElement('span');
-        span.className = 'chat-name small'
-        anchor.appendChild(span);
+        span.className = 'chat-name small';
+        span.setAttribute('contenteditable', 'false');
+        // Make the span take up the remaining space and center its content
+        span.style.flexGrow = '1';
+        span.style.textAlign = 'left';
         span.textContent = room.name;
+
+        const button = document.createElement('button');
+        button.className = 'btn btn-link border-0 p-0 archive-chat-btn';
+        button.type = 'button';
+
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('width', '24');
+        svg.setAttribute('height', '24');
+        svg.setAttribute('viewBox', '0 0 20 20');
+        svg.setAttribute('fill', 'currentColor'); // Change color using CSS
+
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path.setAttribute('d', 'M3.62188 3.07918C3.87597 2.571 4.39537 2.25 4.96353 2.25H13.0365C13.6046 2.25 14.124 2.571 14.3781 3.07918L15.75 5.82295V13.5C15.75 14.7426 14.7426 15.75 13.5 15.75H4.5C3.25736 15.75 2.25 14.7426 2.25 13.5V5.82295L3.62188 3.07918ZM13.0365 3.75H4.96353L4.21353 5.25H13.7865L13.0365 3.75ZM14.25 6.75H3.75V13.5C3.75 13.9142 4.08579 14.25 4.5 14.25H13.5C13.9142 14.25 14.25 13.9142 14.25 13.5V6.75ZM6.75 9C6.75 8.58579 7.08579 8.25 7.5 8.25H10.5C10.9142 8.25 11.25 8.58579 11.25 9C11.25 9.41421 10.9142 9.75 10.5 9.75H7.5C7.08579 9.75 6.75 9.41421 6.75 9Z');
+
+        svg.appendChild(path);
+        button.appendChild(svg);
+
+        // Append span and button to the container
+        container.appendChild(span);
+        container.appendChild(button);
+
+        // Append the container to the anchor
+        anchor.appendChild(container);
+
+        listGroup.appendChild(anchor);
+
         // If the room's ID/UUID matches the current URL, add the 'active-chatroom' class
         if(room.id === chatRoomIdOrUuid) {
             anchor.classList.add('active-chatroom');
-                anchor.addEventListener('click', function(event) {
+            anchor.addEventListener('click', function(event) {
                 event.preventDefault(); // Prevent navigation on click
             })
         };
         listGroup.appendChild(anchor);
         // Add event listener for double-click
-        anchor.addEventListener('dblclick', function() {
-            handleDoubleClickRename(this, room.id);
+        span.addEventListener('dblclick', function() {
+            handleDoubleClickRename(span, room.id);
         });
-        
     });
 }
 
@@ -122,44 +154,13 @@ function fetchChatRooms() {
             }
         })
         .catch(e => {
-            console.error('Fetch error:', e);
+            console.error('Fetch ChatRooms error:', e);
         });
 }
 
 
-function getCurrentChatRoomIdOrUuid() {
-    // Extract and return the current chat room ID or UUID from the URL.
-    const pathSegments = window.location.pathname.split('/');
-    return pathSegments[pathSegments.length - 2]; // Adjust based on your URL structure, assuming the ID or UUID is the second last segment.
-}
-
-
-function handleChatItemClick(event, item) {
-    event.preventDefault(); // Always prevent default to manually control navigation
-
-    const href = item.getAttribute('href');
-    const chatId = href.split('/')[2]; // Extract the chat ID from the href
-
-    // Check for double click
-    if (clickTimer) {
-        clearTimeout(clickTimer);
-        clickTimer = null;
-        // Double-click detected
-        enableRenameMode(item, chatId);
-    } else {
-        clickTimer = setTimeout(() => {
-            clickTimer = null;
-            // Single click action
-            if (chatId !== currentChatRoomId) {
-                window.location.href = href; // Navigate only if different chat room
-            }
-        }, 300); // Adjust threshold as needed
-    }
-}
 async function saveNewName(chatId, newName) {
     // Implement the AJAX call to save the new name to the backend
-    var csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-
     console.log(`Saving new name "${newName}" for chatId ${chatId}`);
     // Example AJAX call (you need to implement this according to your backend)
     fetch('/save_chat_name/', {
@@ -184,50 +185,144 @@ async function saveNewName(chatId, newName) {
 
 
 function handleDoubleClickRename(chatItem, chatId) {
-    const nameElement = chatItem.querySelector('.chat-name');
-    const currentName = nameElement.textContent;
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.value = currentName;
-    input.className = 'form-control chat-name-input';  // TODO: adjust styling
+    const currentName = chatItem.textContent;
+    chatItem.setAttribute("contentEditable", "true");
+    chatItem.focus();
+    // Event listener for pressing Enter key
+    chatItem.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            console.log("Enter")
+            this.blur(); // Triggers the blur event
+        }
+    });
+    chatItem.addEventListener('keydown', function(e) {
+        if (e.key === 'Escape') {
+            console.log("Escape")
+            this.textContent = currentName;
+            this.blur(); // Triggers the blur event
+        }
+    });
 
-    // Replace the name element with the input field
-    chatItem.replaceChild(input, nameElement);
-    // Focus the input and select the text
-    input.focus();
-    input.select();
-
-    // Define behavior for input blur (losing focus)
-    input.addEventListener('blur', function() {
-        const newName = input.value.trim();
+    chatItem.addEventListener('blur', function() {
+        const newName = this.textContent.trim();
         if (newName && newName !== currentName) {
             // Save the new name
             saveNewName(chatId, newName).then(() => {
                 // Update the UI with the new name
-                nameElement.textContent = newName;
-                chatItem.replaceChild(nameElement, input);
+                this.textContent = newName;
             }).catch(error => {
                 console.error('Error updating chat name:', error);
-                // Optionally, revert to the old name on error
-                chatItem.replaceChild(nameElement, input);
             });
         } else {
             // No change or empty name, revert to the old name
-            chatItem.replaceChild(nameElement, input);
-        }
-    });
-    // Event listener for pressing Enter key
-    input.addEventListener('keypress', function(e) {
-        if (e.key === 'Enter') {
-            e.preventDefault(); // Prevent form submission if it's part of a form
-            input.blur(); // Triggers the blur event
-        }
+            this.textContent = currentName;
+        };
     });
 
+    // const input = document.createElement('input');
+    // input.type = 'text';
+    // input.value = currentName;
+    // input.className = 'form-control chat-name-input';  // TODO: adjust styling
+
+    // // Replace the name element with the input field
+    // chatItem.replaceChild(input, nameElement);
+    // // Focus the input and select the text
+    // input.focus();
+    // input.select();
+
+    // input.addEventListener('mousedown', function(e) {
+    //     e.stopPropagation();
+    // });
+    // // Define behavior for input blur (losing focus)
+    // input.addEventListener('click', function(e) {
+    //     e.stopPropagation();
+    // });
+
+
+    // // Event listener for pressing Enter key
+    // input.addEventListener('keydown', function(e) {
+    //     if (e.key === 'Enter') {
+    //         console.log("Enter")
+    //         this.blur(); // Triggers the blur event
+    //     }
+    // });
+    // input.addEventListener('keydown', function(e) {
+    //     if (e.key === 'Escape') {
+    //         console.log("Escape")
+    //         this.value = currentName;
+    //         this.blur(); // Triggers the blur event
+    //     }
+    // });
+    
+    // input.addEventListener('dblclick', function(e) {
+    //     this.focus();
+    //     this.select();
+    // });
+    
+    // input.addEventListener('blur', function() {
+    //     const newName = this.value.trim();
+    //     if (newName && newName !== currentName) {
+    //         // Save the new name
+    //         saveNewName(chatId, newName).then(() => {
+    //             // Update the UI with the new name
+    //             nameElement.textContent = newName;
+    //         }).catch(error => {
+    //             console.error('Error updating chat name:', error);
+    //         });
+    //     };
+    //     chatItem.replaceChild(nameElement, input);
+    // });
 };
 
+
+function hideSidebarElement(element, chatId) {
+
+
+    // Call the backend to hide the chat room
+    fetch(`/archive_chat_room/${chatId}/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // Include CSRF token as needed for Django
+            'X-CSRFToken': csrftoken
+        }
+    }).then(response => {
+        if (response.ok) {
+            // Hide the element on successful response
+            fetchChatRooms();
+        } else {
+            console.error('Failed to archive chat room');
+        }
+    }).catch(error => console.error('Error:', error));
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
+    fetchChatRooms();
     toggleSidebarOnLoad();
     toggleSidebarButtonVisibility();
-    fetchChatRooms();
+    
+    document.getElementById('list-group').addEventListener('click', function(event) {
+        const clickedElement = event.target;
+    
+        // Check if the clicked element or its parent is a hide button
+        if (clickedElement.classList.contains('archive-chat-btn') || clickedElement.closest('.archive-chat-btn')) {
+            event.preventDefault(); // Prevent default anchor action
+            event.stopPropagation();
+            // chatRoomIdOrUuid = currentPath.split('/')[2];
+            const chatId = clickedElement.closest('.list-group-item').getAttribute('data-chat-id');
+            console.log('Hide button clicked for room:', chatId);
+
+            hideSidebarElement(this.parentElement, chatId);
+            // Implement hide logic here
+        }
+        // Check if a chat room item was clicked
+        else if (clickedElement.classList.contains('list-group-item') || clickedElement.closest('.list-group-item')) {
+            const chatId = clickedElement.getAttribute('data-room-id') || clickedElement.closest('.list-group-item').getAttribute('data-chat-id');
+            console.log('Chat room clicked:', chatId);
+            // Implement navigation or other logic here, preventing default if necessary
+            // event.preventDefault();
+        }
+    });
 });
