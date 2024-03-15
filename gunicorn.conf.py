3d774250431f80661985ec30d18c3c2257e83ca2
Alexey Bogomolov <mail@abogomolov.com>
2024-03-16 02:58:33 +0300

    log level

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,15 +1,15 @@
 # Gunicorn configuration file: gunicorn.conf.py
+loglevel = 'error'
 
 # Access log - requests
-accesslog = '/usr/local/var/log/gunicorn-access.log'  # use '-' for stdout
+accesslog = '-'  # Writes access logs to stdout
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 # Error log - server errors
-errorlog = '/usr/local/var/log/gunicorn-error.log'  # use '-' for stdout
+errorlog = '-'  # Writes error logs to stderr
 log_format = '[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s'
 
 # Date format
 log_date_format = '%Y-%m-%d %H:%M:%S'
 
-timeout=120
-
+timeout = 120
