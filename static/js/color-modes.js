ce01ffb0f03894e12ff44b67aa00850710e0d684
Alexey Bogomolov <mail@abogomolov.com>
2024-04-15 10:33:35 +0300

    fallback to the dark theme till light theme support magically arrives

--- static/js/color-modes.js
+++ static/js/color-modes.js
@@ -23,7 +23,7 @@
     if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       document.documentElement.setAttribute('data-bs-theme', 'dark')
     } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
+      document.documentElement.setAttribute('data-bs-theme', 'dark') // temporarily (or permanently) fallback to the dark theme
     }
   }
 
